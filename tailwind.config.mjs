d744e0a19dc9b964d0349b2ca43fc250d055b3eb
Azazek6 <naturaespinola12@gmail.com>
2024-11-20 11:52:11 -0500

    fix responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
 		extend: {},
 	},
 	plugins: [],
