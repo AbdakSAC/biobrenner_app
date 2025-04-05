91a829a138b3a269097d056a43c63eb129fc1925
Azazek6 <naturaespinola12@gmail.com>
2025-04-05 10:55:34 -0500

    changes: contact number

---
+++ src/consts/cards.ts
@@ -0,0 +1,20 @@
+export const CARDS = [
+  {
+    title: "Conviértete en un Generador 💡",
+    description:
+      "Únete a una comunidad de personas que transforman su pasión en ingresos seguros.",
+    link: "https://t.me/generamasoficial ",
+  },
+  {
+    title: "Modalidades de aprendizaje personalizadas 👨‍🏫",
+    description:
+      "Conoce las modalidades de aprendizaje que disponemos y escoge la mejor para ti.",
+    link: "https://wa.link/y9csr2",
+  },
+  {
+    title: "Llénate de información semanal 💻",
+    description:
+      "Ingresa a mi canal de YouTube y obtén información de manera gratuita.",
+    link: "https://www.youtube.com/@brennerveraoficial",
+  },
+];
